13db58b5f11f9906b83859c6c88ceb4b55a8eeac
eric nicolas <eric.e.nicolas@gmail.com>
2018-03-27 15:22:47 -0700

    tuesday2403

--- js/scripts.js
+++ js/scripts.js
@@ -17,13 +17,88 @@ for (i = 3; i < n ; i = i +2){
     primes.push(i);
   }
 }
-$("p").text(primes);
+$("#prime").text(primes);
+}
+
+function factorial(n){
+  console.log("entering function");
+  var output = 1;
+  for ( var i = 1 ; i <= 5 ;i = i+1 ){
+    console.log("entering for");
+    output = output * i;
+  }
+  console.log(output);
+}
+
+  function factorial1(n){
+    console.log("entering function");
+    var output = n;
+    for ( var i = n-1 ; i >= 1 ;i = i-1 ){
+      console.log("entering for");
+      output = output * i;
+    }
+    console.log(output);
+    $("#factorial").text(output);
+
+}
+function wordpuzzle(n){
+
+console.log(n.length);
+var array =["a","e","i","o","u","y"];
+var newarray=[" "];
+var pushed = false;
+for (var i=0; i < n.length; i++){
+  pushed = false;
+  for (var j=0; j < array.length; j++){
+    if(array[j] == n[i]){
+      newarray.push("-");
+      pushed = true;
+    }
+}
+if (pushed == false){
+  console.log(pushed);
+newarray.push(n[i]);
+
+}
+}
+
+console.log(newarray.join(""));
+var array2 = newarray.join("")
+$("#wordpuzzle").text(array2);
+}
+
+function palindrom (n){
+
+var string = n.split(' ').join('');
+console.log(string);
+for (var i = 0; i < string.length; i++){
+    if(string[i] != string[string.length -1-i]){
+
+      return false;
+
+    }
+
+
+}
+return true;
 }
 
 $(document).ready(function(){
   $("#validation").click(function(){
 var n = $("textarea").val();
 prime(n);
+factorial1(n);
+wordpuzzle(n);
+var check = palindrom(n);
+console.log(check);
+if (check == true ){
+  $("#palindrome").text("it is a palindrome");
+}
+if (check == false){
+  $("#palindrome").text("it is not a palindrome");
+}
+
+
 
 });
 });
